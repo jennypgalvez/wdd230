8e3f7aef938721bc075645f649afa41065692be5
jennypgalvez <jennypiril@gmail.com>
2023-12-09 17:24:15 -0600

    modify last-modified js

--- chamber/scripts/last-modified.js
+++ chamber/scripts/last-modified.js
@@ -2,4 +2,3 @@ document.getElementById("currentYear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = new Date(
   document.lastModified
 );
-document.getElementById("timestamp").value = new Date();
